d79c42a3d6a955ce9496312b7a932c65126500c5
Alondra Warren <daisy.codeimmersives.com>
2021-03-03 11:59:46 -0800

    work through console.logging

--- main.js
+++ main.js
@@ -15,11 +15,54 @@ console.log('##### Challenge 1 #####');
 // Hint: Use `if` and a modulus operation.
 
 
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+for (const i=0; i < nums.filter; i++);
+if (nums % 3 === 0) {
+    console.log(nums);
+}
+
 
 console.log('##### Challenge 2 #####');
 // 2.
 //
 // Console out the numbers from 1-20. BACKWARDS.
+
+const numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+const newArr = numArr.reverse().push();
+console.log(newArr);
+
+const newArr2 = numArr.reverse().length -1;
+console.log(newArr2)
+
+const newArr3 = numArr.reverse().length-2;
+console.log(newArr3)
+
+const newArr4 = numArr.reverse().length-3;
+console.log(newArr4);
+
+const newArr5 = numArr.reverse().length-4;
+console.log(newArr5);
+
+const newArr6 = numArr.reverse().length[-5];
+console.log(newArr6);
+
+const newArr7 = numArr.reverse().length-6;
+console.log(newArr7);
+
+const newArr8 = numArr.reverse().length-7;
+console.log(newArr8);
+
+const newArr9 = numArr.reverse().length-8;
+console.log(newArr9);
+
+const newArr10 = numArr.reverse().length-9;
+console.log(newArr10);
+
+const newArr11 = numArr.reverse().length-10;
+console.log(newArr11);
+
+const newArr12 = numArr.reverse().length-11;
+console.log(newArr12);
 
 
 
@@ -56,6 +99,7 @@ console.log('##### Challenge 6 #####');
 // For the string 'Whoa, I am the best!', console out the letters individually,
 // but also capitalize the letters in the third word.
 let string6 = 'Whoa, I am the best!';
+
 
 
 
